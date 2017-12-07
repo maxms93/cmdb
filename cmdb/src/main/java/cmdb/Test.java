b51cfe0c77a64fa59be8debe9b990a7851811403
ART <ART@ART>
2017-12-07 20:39:40 +0100

    Methoden wie getDataFromFuseki, clearAllOnFuseki, writeStuffToFuseki
    hinzugefügt. TODO: getSoftwareFromFuseki (Query verbessern, um alle 
    
    Software mitnehmen zu können)

--- cmdb/src/main/java/cmdb/Test.java
+++ cmdb/src/main/java/cmdb/Test.java
@@ -44,49 +44,77 @@ public class Test {
 		 * accessor.putModel(m);
 		 */
 		
-		getComponentsFromFuseki();
+		getDataFromFuseki();
 
+		clearAllOnFuseki();
+		
 		writeStuffToFuseki();
+	}
+
+	private static void getDataFromFuseki() {
+		ArrayList<CI> listOfCI = getRAMFromFuseki();
+		listOfCI.addAll(getSoftwareFromFuseki());
+		
+		for(CI ci : listOfCI)
+		{
+			System.out.println(ci.toString());
+		}
 	}
 
 	private static void writeStuffToFuseki() {
 		//Demo data
-		ArrayList<CI> myCIs = new ArrayList<CI>();
+		ArrayList<CI> myComponents = new ArrayList<CI>();
 		RAM myRam1 = new RAM(101, "Corsair MEGAAA RAM", "16 TB", "9001 MHz");
 		RAM myRam2 = new RAM(102, "Corsair MEGAAA RAM", "16 TB", "9001 MHz");
-		myCIs.add(myRam1);
-		myCIs.add(myRam2);
-		Server myServer = new Server(1, "myServer", true, false, myCIs);
+		myComponents.add(myRam1);
+		myComponents.add(myRam2);
+		Server myServer = new Server(1, "myServer", true, false, myComponents);
+		
+		ArrayList<CI> mySoftware = new ArrayList<CI>();
+		ApplicationSoftware myAS = new ApplicationSoftware(80, "SAP", 9001);
+		SystemSoftware mySS = new SystemSoftware(90, "Windows 10", true);
+		mySoftware.add(myAS);
+		mySoftware.add(mySS);
+		Person myPerson = new Person(990, "123456789", "Hans", "Huber", mySoftware); 
 		//End Demo data
 		
 		ArrayList<Server> myServers = new ArrayList<Server>();
+		ArrayList<Person> myPeople = new ArrayList<Person>();
 		myServers.add(myServer);
+		myPeople.add(myPerson);
 		
+		writeServersToFuseki(myServers);
+		writePeopleToFuseki(myPeople);
+	}
+
+	private static void writeServersToFuseki(ArrayList<Server> myServers) {
 		for(Server s : myServers)
 		{
-			String nameNew = "";
-	        
-			nameNew = s.getBezeichnung().replace(" ", "_");
+			String nameNew = s.getBezeichnung().replace(" ", "_");
 			UpdateRequest update = UpdateFactory.create(
             		"PREFIX prop: <http://artmayr.com/property/>\n" +
                     "PREFIX ont: <http://artmayr.com/ontology/>\n" +
                     "INSERT DATA\n{\n<http://artmayr.com/resource/" + nameNew + "> " +
-                    "prop:name \"" + myServer.getBezeichnung() + "\" ;\n" +
-                    "prop:isVirtualized \"" + myServer.isVirtualized() + "\" ;\n" +
-                    "prop:isSharedServer \"" + myServer.isSharedServer() + "\".\n}");
+                    "prop:id \"" + s.getId() + "\" ;\n" +
+                    "prop:type \"" + s.getType() + "\" ;\n" +
+                    "prop:name \"" + s.getBezeichnung() + "\" ;\n" +
+                    "prop:isVirtualized \"" + s.isVirtualized() + "\" ;\n" +
+                    "prop:isSharedServer \"" + s.isSharedServer() + "\".\n}");
 		    UpdateProcessor processor = UpdateExecutionFactory.createRemote(update, updateEndPoint);
 		    processor.execute();
 
-			if(s.getlComponents() != null)
+			if(s.getListComponents() != null)
 			{			
-				for(CI comp : s.getlComponents())
+				for(CI comp : s.getListComponents())
 				{
 					if(comp instanceof RAM)
 					{
 						update = UpdateFactory.create(
 				        		"PREFIX prop: <http://artmayr.com/property/>\n" +
 				                "PREFIX ont: <http://artmayr.com/ontology/>\n" +
-				                "INSERT DATA\n{\n<http://artmayr.com/resource/" + comp.getId() + "/" + comp.getBezeichnung().replace(" ", "_") + "> " +
+				                "INSERT DATA\n{\n<http://artmayr.com/resource/" + comp.getId() + "_" + comp.getBezeichnung().replace(" ", "_") + "> " +
+				                "prop:id \"" + comp.getId() + "\" ;\n" +
+				                "prop:type \"" + comp.getType() + "\" ;\n" +
 				                "prop:name \"" + comp.getBezeichnung() + "\" ;\n" +
 				                "prop:groesse \"" + ((RAM) comp).getGroesse() + "\" ;\n" +
 				                "prop:taktung \"" + ((RAM) comp).getTaktung() + "\" .\n}");
@@ -97,7 +125,74 @@ public class Test {
 				        		"PREFIX prop: <http://artmayr.com/property/>\n" +
 				                "PREFIX ont: <http://artmayr.com/ontology/>\n" +
 				                "INSERT DATA\n{\n<http://artmayr.com/resource/" + nameNew + ">" +
-				                "prop:hasComponent \"" + comp.getId() + "/" + comp.getBezeichnung().replace(" ", "_") + "\" .\n}");
+				                "prop:hasComponent \"" + comp.getId() + "_" + comp.getBezeichnung().replace(" ", "_") + "\" .\n}");
+					    processor = UpdateExecutionFactory.createRemote(update, updateEndPoint);
+					    processor.execute();
+					}
+				}
+			}
+		}
+	}
+	
+	private static void writePeopleToFuseki(ArrayList<Person> myPeople) {
+		for(Person p : myPeople)
+		{
+			String nameNew = p.getBezeichnung().replace(" ", "_");
+			UpdateRequest update = UpdateFactory.create(
+            		"PREFIX prop: <http://artmayr.com/property/>\n" +
+                    "PREFIX ont: <http://artmayr.com/ontology/>\n" +
+                    "INSERT DATA\n{\n<http://artmayr.com/resource/" + p.getId() + "_" + nameNew + "> " +
+                    "prop:id \"" + p.getId() + "\" ;\n" +
+                    "prop:type \"" + p.getType() + "\" ;\n" +
+                    "prop:name \"" + p.getBezeichnung() + "\" ;\n" +
+                    "prop:vorname \"" + p.getVorname() + "\" ;\n" +
+                    "prop:nachname \"" + p.getNachname() + "\".\n}");
+		    UpdateProcessor processor = UpdateExecutionFactory.createRemote(update, updateEndPoint);
+		    processor.execute();
+
+			if(p.getListUsage() != null)
+			{
+				for(CI usage : p.getListUsage())
+				{
+					if(usage instanceof ApplicationSoftware)
+					{
+						update = UpdateFactory.create(
+				        		"PREFIX prop: <http://artmayr.com/property/>\n" +
+				                "PREFIX ont: <http://artmayr.com/ontology/>\n" +
+				                "INSERT DATA\n{\n<http://artmayr.com/resource/" + usage.getId() + "_" + usage.getBezeichnung().replace(" ", "_") + "> " +
+				                "prop:id \"" + usage.getId() + "\" ;\n" +
+				                "prop:type \"" + usage.getType() + "\" ;\n" +
+				                "prop:name \"" + usage.getBezeichnung() + "\" ;\n" +
+				                "prop:linesOfCode \"" + ((ApplicationSoftware) usage).getLinesOfCode() + "\" .\n}");
+					    processor = UpdateExecutionFactory.createRemote(update, updateEndPoint);
+					    processor.execute();
+					    
+					    update = UpdateFactory.create(
+				        		"PREFIX prop: <http://artmayr.com/property/>\n" +
+				                "PREFIX ont: <http://artmayr.com/ontology/>\n" +
+				                "INSERT DATA\n{\n<http://artmayr.com/resource/" + p.getId() + "_" + nameNew + ">" +
+				                "prop:isUsing \"" + usage.getId() + "_" + usage.getBezeichnung().replace(" ", "_") + "\" .\n}");
+					    processor = UpdateExecutionFactory.createRemote(update, updateEndPoint);
+					    processor.execute();
+					}
+					if(usage instanceof SystemSoftware)
+					{
+						update = UpdateFactory.create(
+				        		"PREFIX prop: <http://artmayr.com/property/>\n" +
+				                "PREFIX ont: <http://artmayr.com/ontology/>\n" +
+				                "INSERT DATA\n{\n<http://artmayr.com/resource/" + usage.getId() + "_" + usage.getBezeichnung().replace(" ", "_") + "> " +
+				                "prop:id \"" + usage.getId() + "\" ;\n" +
+				                "prop:type \"" + usage.getType() + "\" ;\n" +
+				                "prop:name \"" + usage.getBezeichnung() + "\" ;\n" +
+				                "prop:os \"" + ((SystemSoftware) usage).isOS() + "\" .\n}");
+					    processor = UpdateExecutionFactory.createRemote(update, updateEndPoint);
+					    processor.execute();
+					    
+					    update = UpdateFactory.create(
+				        		"PREFIX prop: <http://artmayr.com/property/>\n" +
+				                "PREFIX ont: <http://artmayr.com/ontology/>\n" +
+				                "INSERT DATA\n{\n<http://artmayr.com/resource/" + p.getId() + "_" + nameNew + ">" +
+				                "prop:isUsing \"" + usage.getId() + "_" + usage.getBezeichnung().replace(" ", "_") + "\" .\n}");
 					    processor = UpdateExecutionFactory.createRemote(update, updateEndPoint);
 					    processor.execute();
 					}
@@ -106,37 +201,108 @@ public class Test {
 		}
 	}
 
-	private static void getComponentsFromFuseki() {
+	private static void clearAllOnFuseki() {
+		UpdateRequest delete = UpdateFactory.create(
+                "CLEAR ALL");
+	    UpdateProcessor processor = UpdateExecutionFactory.createRemote(delete, updateEndPoint);
+	    processor.execute();
+	}
+
+	private static ArrayList<CI> getRAMFromFuseki() {
+		ArrayList<CI> listRAM = null;
 		ResultSet result;
 		ParameterizedSparqlString componentQuery = new ParameterizedSparqlString("" +
 		        "PREFIX ont: <http://artmayr.com/ontology/>\n" +
 		        "PREFIX prop: <http://artmayr.com/property/>\n" +
 		        "SELECT\n" +
-		        "?bez  ?groesse ?taktung \n" +
+		        "?id ?type ?bez ?groesse ?taktung \n" +
 		        "WHERE {\n" +
-		        "?name prop:name ?bez . \n" +
+		        "?x prop:id ?id . \n" +
+		        "?x prop:type ?type . \n" +
+		        "?x prop:name ?bez . \n" +
+		        "?x prop:groesse ?groesse . \n" +
+		        "?x prop:taktung ?taktung . \n" +
 		        "}\n"
 		);
 		QueryExecution exec = QueryExecutionFactory.sparqlService(queryEndPoint, componentQuery.asQuery());
 		try {
 		    result = exec.execSelect();
-		    ArrayList<CI> lCI = new ArrayList<CI>();
+		    listRAM = new ArrayList<CI>();
 		    while (result.hasNext()) {
 		        QuerySolution nextSolution = result.next();
 
+		        RDFNode id = nextSolution.get("id");
+		        RDFNode type = nextSolution.get("type");
 		        RDFNode bezeichnung = nextSolution.get("bez");
 		        RDFNode groesse = nextSolution.get("groesse");
 		        RDFNode taktung = nextSolution.get("taktung");
 
 		        RAM tempRAM = new RAM();
+		        tempRAM.setId(id.asLiteral().getInt());
+		        tempRAM.setType(type.toString());
 		        tempRAM.setBezeichnung(bezeichnung.toString());
 		        tempRAM.setGroesse(groesse == null ? "" : groesse.toString());
 		        tempRAM.setTaktung(taktung == null ? "" : taktung.toString());
-		        lCI.add(tempRAM);
-		        System.out.println(tempRAM.getBezeichnung().toString());
+		        listRAM.add(tempRAM);
 		    }
 		} finally {
 		    exec.close();
 		}
+		return listRAM;
+	}
+	
+	private static ArrayList<CI> getSoftwareFromFuseki() {
+		ArrayList<CI> listSoftware = null;
+		ResultSet result;
+		ParameterizedSparqlString componentQuery = new ParameterizedSparqlString("" +
+		        "PREFIX ont: <http://artmayr.com/ontology/>\n" +
+		        "PREFIX prop: <http://artmayr.com/property/>\n" +
+		        "SELECT\n" +
+		        "?id ?type ?bez ?linesOfCode ?os \n" +
+		        "WHERE {\n" +
+		        "?x prop:id ?id . \n" +
+		        "?x prop:type ?type . \n" +
+		        "?x prop:name ?bez . \n" +
+		        //"?x prop:linesOfCode ?linesOfCode . \n" +
+		        //"?x prop:os ?os . \n" +
+		        //"OPTIONAL {?x prop:os ?os . \n ?x prop:linesOfCode ?linesOfCode . } \n" +
+		        "FILTER (REGEX(?type, 'Software')) . \n" +
+		        "}\n"
+		);
+		QueryExecution exec = QueryExecutionFactory.sparqlService(queryEndPoint, componentQuery.asQuery());
+		try {
+		    result = exec.execSelect();
+		    listSoftware = new ArrayList<CI>();
+		    while (result.hasNext()) {
+		        QuerySolution nextSolution = result.next();
+		        
+		        RDFNode id = nextSolution.get("id");
+		        RDFNode bezeichnung = nextSolution.get("bez");
+		        RDFNode type = nextSolution.get("type");
+		        if(type.toString().equals("SystemSoftware"))
+		        {
+		        	SystemSoftware ss = new SystemSoftware();
+		        	ss.setId(id.asLiteral().getInt());
+		        	ss.setBezeichnung(bezeichnung.toString());
+		        	ss.setType(type.toString());
+		        	//ss.setOS(nextSolution.get("isOS").asLiteral().getBoolean());
+
+			        listSoftware.add(ss);
+		        }
+		        if(type.toString().equals("ApplicationSoftware"))
+		        {
+		        	ApplicationSoftware as = new ApplicationSoftware();
+		        	as.setId(id.asLiteral().getInt());
+		        	as.setBezeichnung(bezeichnung.toString());
+		        	as.setType(type.toString());
+		        	//as.setLinesOfCode(nextSolution.get("linesOfCode").asLiteral().getInt());
+		        	
+			        listSoftware.add(as);
+		        }
+		    }
+		} finally {
+		    exec.close();
+		}
+		return listSoftware;
 	}
 }
