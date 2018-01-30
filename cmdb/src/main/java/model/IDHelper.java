571e1fcc8c78d2ae40badb563eac112cb22b9c12
ARTilol <p.artmayr@gmail.com>
2018-01-30 11:13:08 +0100

    Handeln von IDs verbessert.

--- cmdb/src/main/java/model/IDHelper.java
+++ cmdb/src/main/java/model/IDHelper.java
@@ -4,7 +4,16 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class IDHelper {
 	private final static AtomicInteger c = new AtomicInteger(0);
-    public static int getID() {
-        return c.incrementAndGet();
-    }
+
+	public static int getID() {
+		return c.incrementAndGet();
+	}
+
+	public static void setIDFrontend(int id) {
+		c.set(id);
+	}
+	
+	public static int getIDFrontend() {
+		return c.get();
+	}
 }
